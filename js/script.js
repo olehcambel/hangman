494c312765dd4b8b52815363c29f9ea7f76e19e9
olehcambel <nickcambel5@gmail.com>
2018-07-25 20:02:13 +0300

    add buttons via createButtons

---
+++ js/script.js
@@ -0,0 +1,43 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const alphabet = [
+    'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'
+  ];
+
+  let categories, //Themes
+    chosenCategory, 
+    word, 
+    guesses = [], // array of user's attempts
+    lives,
+    counter, // amount of guesses letters
+    space // Space in word
+
+  let showLives = document.querySelector('#mylives'),
+    showCategory = document.querySelector('#categoryName'),
+    getHint = document.querySelector('#hint'),
+    showClue = document.querySelector('#clue'),
+    reset = document.querySelector('#reset')
+
+  function createButtons() {
+    let myButtons = document.querySelector('#buttons'),
+    letters = document.createElement('ul')
+
+  for (let i = 0; i < alphabet.length; i++) {
+    letters.id = 'alphabet'
+    let list = document.createElement('li')
+    list.innerHTML = alphabet[i]  
+    myButtons.appendChild(letters)
+    letters.appendChild(list)
+  
+  }
+}
+
+createButtons()
+
+  // for (let i = 0; i < alphabet.length; i++) {
+  //   letters.id = 'alphabet'
+  //   let list = document.createElement('li')
+  //   list.innerHTML = alphabet[i]  
+  //   myButtons.appendChild(list)
+  // }
+
+});
